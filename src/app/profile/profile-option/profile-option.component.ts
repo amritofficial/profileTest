b0939c2dc0d20adca2514624dd10f07b2cd32e95
AmritOfficial <itsamrit1996@gmail.com>
2018-07-15 15:44:12 -0400

    profile-options separated into components

---
+++ src/app/profile/profile-option/profile-option.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'profile-option',
+  templateUrl: './profile-option.component.html',
+  styleUrls: ['./profile-option.component.css', '../../../assets/css/blocks.css', '../../../assets/css/theme-styles.css']
+})
+export class ProfileOptionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
