7f5239bf30225c5c0ca1e530a4a9d32cdb472385
AmritOfficial <itsamrit1996@gmail.com>
2018-07-29 17:32:31 -0400

    Implemented Google Maps with Geolocation and User Tracking

--- src/app/devfinder-portal/devfinder-portal.component.ts
+++ src/app/devfinder-portal/devfinder-portal.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {} from '@types/googlemaps';
 
 @Component({
   selector: 'app-devfinder-portal',
@@ -6,10 +7,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./devfinder-portal.component.css']
 })
 export class DevfinderPortalComponent implements OnInit {
+  @ViewChild('gmap') gmapElement: any;
+  map: google.maps.Map;
+
+  currentLat: any;
+  currentLong: any;
+
+  marker: google.maps.Marker;
+
+  isTracking = false;
 
   constructor() { }
 
   ngOnInit() {
+    var mapProp = {
+      center: new google.maps.LatLng(43.648647, -79.727653),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
+
+
+  setMapType(mapTypeId: string) {
+    this.map.setMapTypeId(mapTypeId)
+  }
+
+  findMe() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.showPosition(position);
+      });
+    } else {
+      alert("This application requires to access Geolocation for the best experience");
+    }
+  }
+
+  showPosition(position) {
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if(!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Found you!'
+      });
+    } else {
+      this.marker.setPosition(location);
+    }
+  }
+
+  trackMe() {
+    if (navigator.geolocation) {
+      this.isTracking = true;
+      navigator.geolocation.watchPosition((position) => {
+        this.showTrackingPosition(position);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
+  }
+
+  showTrackingPosition(position) {
+    console.log(`tracking postion:  ${position.coords.latitude} - ${position.coords.longitude}`);
+    this.currentLat = position.coords.latitude;
+    this.currentLong = position.coords.longitude;
+
+    let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    this.map.panTo(location);
+
+    if (!this.marker) {
+      this.marker = new google.maps.Marker({
+        position: location,
+        map: this.map,
+        title: 'Got you!'
+      });
+    }
+    else {
+      this.marker.setPosition(location);
+    }
+  }
+
+
 
 }
