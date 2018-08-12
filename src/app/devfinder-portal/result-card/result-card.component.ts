0b4b4ee4570c28133caece9f25a527b7960370f1
AmritOfficial <itsamrit1996@gmail.com>
2018-08-12 17:42:19 -0400

    Result-Card component created and implemented

---
+++ src/app/devfinder-portal/result-card/result-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'result-card',
+  templateUrl: './result-card.component.html',
+  styleUrls: ['./result-card.component.css']
+})
+export class ResultCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
