9e637d6bc406be4061c62d48ce87bb9e6f19af5e
amritofficial <itsamrit1996@gmail.com>
2018-09-11 15:30:44 -0400

    created a list of users in the sidebar

--- src/app/user-list-bar/user-list-bar.component.ts
+++ src/app/user-list-bar/user-list-bar.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../shared/services/firebase.service';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { User } from '../shared/models/user';
 
 @Component({
   selector: 'user-list-bar',
   templateUrl: './user-list-bar.component.html',
-  styleUrls: ['./user-list-bar.component.css']
+  styleUrls: ['./user-list-bar.component.css', '../../assets/css/blocks.css', '../../assets/css/theme-styles.css']
 })
 export class UserListBarComponent implements OnInit {
+  private ngUnsubscribe = new Subject();
+  loading: boolean;
+  users: User[];
 
-  constructor() { }
+  constructor(private firebaseService: FirebaseService) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.firebaseService.getAllFireUsers()
+      .pipe(takeUntil(this.ngUnsubscribe)).subscribe((users: User[]) => {
+        this.users = users;
+        console.log("LIST OF USERS: ");
+        console.log(this.users);
+        this.loading = false;
+      });
   }
 
 }
