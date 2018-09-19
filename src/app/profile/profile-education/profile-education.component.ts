087a9e21fe202cdcf9249ebc01178877e932f443
amritofficial <itsamrit1996@gmail.com>
2018-09-18 21:29:59 -0400

    created an instant refresh of new education addition to list

--- src/app/profile/profile-education/profile-education.component.ts
+++ src/app/profile/profile-education/profile-education.component.ts
@@ -6,9 +6,9 @@ import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 
 @Component({
-  selector: 'profile-education',
-  templateUrl: './profile-education.component.html',
-  styleUrls: ['./profile-education.component.css', '../../../assets/css/blocks.css', '../../../assets/css/theme-styles.css']
+    selector: 'profile-education',
+    templateUrl: './profile-education.component.html',
+    styleUrls: ['./profile-education.component.css', '../../../assets/css/blocks.css', '../../../assets/css/theme-styles.css']
 })
 export class ProfileEducationComponent implements OnInit {
   private ngUnsubscribe = new Subject();
@@ -21,66 +21,60 @@ export class ProfileEducationComponent implements OnInit {
   loadingProfile: boolean = false;
 
   constructor(private profileService: ProfileService,
-    private userService: UserService) { }
+      private userService: UserService) { }
 
   ngOnInit() {
-    this.loadingProfile = true;
-    this.profileService.getCurrentUserEducation(this.userService.currentUser.userId)
-      .pipe(takeUntil(this.ngUnsubscribe)).subscribe((education) => {
-
-        console.log(JSON.stringify(education.results));
-        let objectArray = JSON.stringify(education.results);
-        this.educationArray = JSON.parse(objectArray);
-        console.log(this.educationArray);
-        // console.log(this.educationArray);
-        this.filterEducation();
-        // console.log(this.educationArray);
-        // console.log(education);
-      });
-
-    // this.getCurrentUserEducation();
+      this.loadingProfile = true;
+      this.getCurrentUserEducation();
   }
 
   saveEducation() {
-    console.log('Save Education');
-    this.savingEducation = true;
-    this.addEducation.userId = this.userService.currentUser.userId;
-    this.profileService.saveCurrentUserEducation(this.addEducation)
-      .pipe(takeUntil(this.ngUnsubscribe)).subscribe((education) => {
-        console.log(education);
-        this.savingEducation = false;
-        this.closeAddEducationForm();
-        console.log('Sava Successful');
-      });
-    // console.log(this.addEducation);
+      console.log('Save Education');
+      this.savingEducation = true;
+      this.addEducation.userId = this.userService.currentUser.userId;
+      this.profileService.saveCurrentUserEducation(this.addEducation)
+          .pipe(takeUntil(this.ngUnsubscribe)).subscribe((education) => {
+              console.log(education);
+              // this.filterdEducationArray.push(education);
+              this.savingEducation = false;
+              this.getCurrentUserEducation();
+              this.closeAddEducationForm();
+              console.log('Sava Successful');
+          });
+      // console.log(this.addEducation);
   }
 
   openAddEducationForm(event) {
-    this.addProfileEducation = true;
+      this.addProfileEducation = true;
   }
 
   closeAddEducationForm() {
-    this.addProfileEducation = false;
+      this.addProfileEducation = false;
   }
 
   getCurrentUserEducation() {
-    this.profileService.getCurrentUserEducation(this.userService.currentUser.userId)
-      .pipe(takeUntil(this.ngUnsubscribe)).subscribe((education: Education[]) => {
-        this.educationArray = education;
-        console.log(this.educationArray);
-        // console.log(education);
+      this.profileService.getCurrentUserEducation(this.userService.currentUser.userId).subscribe((education) => {
+
+          console.log(JSON.stringify(education.results));
+          let objectArray = JSON.stringify(education.results);
+          this.educationArray = JSON.parse(objectArray);
+          console.log(this.educationArray);
+          // console.log(this.educationArray);
+          this.filterEducation();
+          // console.log(this.educationArray);
+          // console.log(education);
       });
   }
 
   filterEducation() {
-    let userId = String(window.sessionStorage.getItem('current_user_id'));
-    // console.log(window.sessionStorage.getItem('current_user_id'));
-    // console.log(userId);
-    console.log(this.userService.currentUser.userId)
-    this.filterdEducationArray = this.educationArray.filter(education => education.userId === userId);
-    console.log(this.filterdEducationArray);
-    this.loadingProfile = false;
-    console.log('filtered');
+      let userId = String(window.sessionStorage.getItem('current_user_id'));
+      // console.log(window.sessionStorage.getItem('current_user_id'));
+      // console.log(userId);
+      console.log(this.userService.currentUser.userId)
+      this.filterdEducationArray = this.educationArray.filter(education => education.userId === userId);
+      console.log(this.filterdEducationArray);
+      this.loadingProfile = false;
+      console.log('filtered');
   }
 
 }
