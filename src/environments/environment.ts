7d41cf848ece07d3ec2c09760985d63991ff478d
amritofficial <itsamrit1996@gmail.com>
2018-09-21 14:38:23 -0400

    updated environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,24 +6,24 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyAky_B0vhIrEzDkGBDosWO5evVB_-HHNII",
-    authDomain: "devfinder-chat.firebaseapp.com",
-    databaseURL: "https://devfinder-chat.firebaseio.com",
-    projectId: "devfinder-chat",
-    storageBucket: "devfinder-chat.appspot.com",
-    messagingSenderId: "1043034343322"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    databaseURL: "DB_URL",
+    projectId: "_PROJ_ID",
+    storageBucket: "BUCKET_URL",
+    messagingSenderId: "SENDER_ID"
   },
   parseServer: {
-    databaseURI: 'mongodb://<dbuser>:<dbpassword>@ds251002.mlab.com:51002/heroku_bpbtr5cw',
-    appId: 'angular-parse-chat',
-    newAppId: '13161197ab22343bdb876503d3edf547cdc4b8bf',
-    masterKey: 'AmritSinghOfficialFullStackdeveloper987', //Add your master key here. Keep it secret!
-    serverURL:'https://angular-parse-chat.herokuapp.com/parse',  // Don't forget to change to https if needed
-    clientKey: 'AmritSinghOfficialFullStackdeveloper',
-    restAPIKey: 'DevFinderOfficialAPI',
-    restNewKey: '412bf95c90ea08fc4c95cbdd75a404bea254872e',
+    databaseURI: 'DB_URL',
+    appId: 'APP_ID',
+    newAppId: 'APP_ID',
+    masterKey: 'MASTER_KEY', //Add your master key here. Keep it secret!
+    serverURL:'SERVER_URL',  // Don't forget to change to https if needed
+    clientKey: 'CLIENT_KEY',
+    restAPIKey: 'REST_API_KEY',
+    restNewKey: 'REST_API_KEY',
     liveQuery: {
       classNames: ["Posts", "Comments", "chat"] // List of classes to support for query subscriptions
     }
-  }
+}
 };
