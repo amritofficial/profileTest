58c1b728f273e47e426fd4fd1ce87b63528a1f41
amritofficial <itsamrit1996@gmail.com>
2018-10-04 14:20:55 -0400

    created a separate comonent for devfinder-question-tag

---
+++ src/app/devfinder-portal/devfinder-tags/tag/tag.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'devfinder-question-tag',
+  templateUrl: './tag.component.html',
+  styleUrls: ['./tag.component.css']
+})
+export class TagComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
