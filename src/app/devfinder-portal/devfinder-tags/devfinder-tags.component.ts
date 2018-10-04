cfe75f78ba5ec42869da26aeddc6584b3070c3fe
amritofficial <itsamrit1996@gmail.com>
2018-10-04 15:16:58 -0400

    created the grid to loop the devfinder tags

--- src/app/devfinder-portal/devfinder-tags/devfinder-tags.component.ts
+++ src/app/devfinder-portal/devfinder-tags/devfinder-tags.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DevFinderTag } from '../../shared/models/devfinder-tag';
 
 @Component({
   selector: 'devfinder-tags',
@@ -6,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./devfinder-tags.component.css']
 })
 export class DevfinderTagsComponent implements OnInit {
+  devFinderTagsArray: DevFinderTag[] = [
+    {
+      tagDescription: 'Lorem Ipsum',
+      tagName: 'Javacript',
+      tagOpenIssues: 20,
+      vacinityDevelopers: 1
+    },
+    {
+      tagDescription: 'Lorem Ipsum',
+      tagName: 'Angular',
+      tagOpenIssues: 20,
+      vacinityDevelopers: 1
+    },
+    {
+      tagDescription: 'Lorem Ipsum',
+      tagName: 'Typescript',
+      tagOpenIssues: 20,
+      vacinityDevelopers: 1
+    },
+    {
+      tagDescription: 'Lorem Ipsum',
+      tagName: 'Java',
+      tagOpenIssues: 20,
+      vacinityDevelopers: 1
+    },
+    {
+      tagDescription: 'Lorem Ipsum',
+      tagName: 'C#',
+      tagOpenIssues: 20,
+      vacinityDevelopers: 1
+    }
+  ]
 
   constructor() { }
 
---
+++ src/app/shared/models/devfinder-tag.ts
@@ -0,0 +1,6 @@
+export class DevFinderTag {
+    public tagName: string;
+    public tagDescription: string;
+    public tagOpenIssues: any;
+    public vacinityDevelopers: any;
+}
