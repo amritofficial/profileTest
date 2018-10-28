83f1535e37e2d54de6544e868cfdc1bd62acadb9
amritofficial <itsamrit1996@gmail.com>
2018-10-28 19:09:30 -0400

    created optional video and image upload fields in feed model

--- src/app/post-feed-box/post-feed-box.component.ts
+++ src/app/post-feed-box/post-feed-box.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from 'app/shared/models/user';
+import { PostService } from 'app/shared/services/post.service';
+import { Feed } from 'app/shared/models/feed';
 
 @Component({
   selector: 'post-feed-box',
@@ -8,10 +10,47 @@ import { User } from 'app/shared/models/user';
 })
 export class PostFeedBoxComponent implements OnInit {
   @Input() currentUser: User;
+  feedImageSrc: string;
+  onErrorImage: string = "https://www.cambridgesuitestoronto.com/resourcefiles/attractionsmallimages/cn-tower-toronto-ontario-th.jpg";
+  feedBody: string = '';
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
   }
 
+  fileChangeListener($event) {
+    // this.imageChangeEvent = $event;
+    var image: any = new Image();
+    var file: File = $event.target.files[0];
+    var myReader: FileReader = new FileReader();
+    var that = this;
+    var tempImage;
+    myReader.onloadend =  (loadEvent: any) => {
+      image.src = loadEvent.target.result;
+      tempImage = loadEvent.target.result;
+      this.feedImageSrc = tempImage;
+      // that.cropper.setImage(image);
+    };
+    // this.imageSelected = true;
+    myReader.readAsDataURL(file);
+    // this.croppedImage = data2.image;
+  }
+
+  postFeed() {
+    let feed: Feed = {
+      comment: [],
+      feedBody: this.feedBody,
+      feedId: null,
+      feedImageUrl: "Image should be stored here",
+      like: [],
+      timeStamp: new Date().getTime(),
+      user: this.currentUser
+    }
+
+    this.postService.storeFeed(feed, this.currentUser.userId).then((data) => {
+      this.feedBody = "";
+      console.log("feedStored");
+    });
+  }
 }
--- src/app/shared/models/feed.ts
+++ src/app/shared/models/feed.ts
@@ -7,6 +7,8 @@ export class Feed {
     public timeStamp: any;
     public feedBody: any;
     public feedId: any;
+    public feedImageUrl?: any;
+    public feedVideoUrl?: any;
     public like: Like[];
     public comment: Comment[];
 }
