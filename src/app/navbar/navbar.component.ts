ec5f73fe55d22947d520154a277da91696b0f316
amritofficial <itsamrit1996@gmail.com>
2018-10-28 20:48:11 -0400

    created timestamp filtering of 4 days old Feed Post notification on navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -44,7 +44,7 @@ export class NavbarComponent implements OnInit, OnChanges, OnDestroy {
     status: '',
     to: this.userService.user
   }];
-  
+
   sentLinkRequestArrayWithSentStatus: LinkRequest[] = [];
 
   approvedRequestArray: any[] = [];
@@ -182,13 +182,17 @@ export class NavbarComponent implements OnInit, OnChanges, OnDestroy {
           let object = JSON.parse(JSON.stringify(feed[i]));
           this.feedList = [];
           for (var x in object) {
+            let time = new Date().getTime() - object[x].timeStamp;
             if (object[x].user.userId !== this.userService.currentUser.userId) {
               this.feedList.push(object[x]);
-              this.globalFeednotificationList.push(object[x]);
+              if (time < fourDaysTimeStamp) {
+                this.globalFeednotificationList.push(object[x]);
+              }
             }
-            console.log(object[x].timeStamp);
+            console.log((new Date().getTime()) - object[x].timeStamp);
           }
         }
+        console.log("Four Day TimeStamp: " + fourDaysTimeStamp);
       }
       // this.getNotificationCount();
       console.log(this.feedList);
